b6d595b07610d874ddeea9ec69e0238148d38094
krouspy <knjlau1@gmail.com>
2022-01-13 06:08:28 +0100

    add ropsten accounts and set mocha timeout

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,8 +9,6 @@ import 'solidity-coverage';
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -19,16 +17,16 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
   solidity: '0.4.21',
   networks: {
     ropsten: {
-      url: process.env.ROPSTEN_URL || '',
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}` || '',
       accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        process.env.PRIVATE_KEY !== undefined &&
+        process.env.PRIVATE_KEY_ACCOMPLICE !== undefined
+          ? [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_ACCOMPLICE]
+          : [],
     },
   },
   gasReporter: {
@@ -37,6 +35,9 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  mocha: {
+    timeout: 60000,
   },
 };
 
